71c9fcc0751cd844735b22dff179a855bcff98a4
Jin409 <sualng123@gmail.com>
2023-05-19 02:57:09 +0900

    feat : 음식 상세 조회 기능 구현 #17

--- src/main/java/sopt/cds/baemin/controller/StoreController.java
+++ src/main/java/sopt/cds/baemin/controller/StoreController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sopt.cds.baemin.common.dto.ApiResponse;
@@ -13,11 +14,12 @@ import sopt.cds.baemin.dto.store.StoreInfoDto;
 import sopt.cds.baemin.service.StoreService;
 
 @RestController
+@RequestMapping("/store")
 @RequiredArgsConstructor
 public class StoreController {
     private final StoreService storeService;
 
-    @GetMapping("/store")
+    @GetMapping
     public ApiResponse<List<StoreInfoDto>> findStores(@RequestParam(value = "type") @Nullable Long storeTypeId) {
         List<StoreInfoDto> stores = storeService.findStores(storeTypeId);
         return ApiResponse.success(STORES_GET_SUCCESS, stores);
--- src/main/java/sopt/cds/baemin/exception/Success.java
+++ src/main/java/sopt/cds/baemin/exception/Success.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@AllArgsConstructor(access =  AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Success {
 
     /**
@@ -14,7 +14,8 @@ public enum Success {
      */
     SIGNUP_SUCCESS(HttpStatus.OK, "리뷰 조회에 성공했습니다.", true),
     ORDER_SUCCESS(HttpStatus.OK, "주문 또는 삭제 성공.", true),
-    STORES_GET_SUCCESS(HttpStatus.OK, "가게 목록 조회에 성공했습니다.", true)
+    STORES_GET_SUCCESS(HttpStatus.OK, "가게 목록 조회에 성공했습니다.", true),
+    FOOD_GET_SUCCESS(HttpStatus.OK, "음식 조회에 성공했습니다.", true),
     ;
 
     private final HttpStatus httpStatus;
