aedabf9809b5d91393094928e1f425ba2aad2b22
JUN <chltmdwns96@naver.com>
2023-05-24 21:21:01 +0900

    feat: 서버 오류 발생 시, Slack 메시지 알림 기능 구현 #23

--- src/main/java/sopt/cds/baemin/common/advice/ControllerExceptionAdvice.java
+++ src/main/java/sopt/cds/baemin/common/advice/ControllerExceptionAdvice.java
@@ -1,7 +1,9 @@
 package sopt.cds.baemin.common.advice;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sopt.cds.baemin.common.dto.ApiResponse;
 import sopt.cds.baemin.exception.Error;
 import sopt.cds.baemin.exception.model.CustomException;
+import sopt.cds.baemin.tools.slack.SlackHelper;
 
 import java.util.Objects;
 
 @RestControllerAdvice
+@Component
+@RequiredArgsConstructor
 public class ControllerExceptionAdvice {
+    private final SlackHelper slackHelper;
 
     /**
      * 400 BAD_REQUEST
@@ -24,6 +30,7 @@ public class ControllerExceptionAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ApiResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         FieldError fieldError = Objects.requireNonNull(e.getFieldError());
+
         return ApiResponse.error(Error.REQUEST_VALIDATION_EXCEPTION, String.format("%s. (%s)", fieldError.getDefaultMessage(), fieldError.getField()));
     }
 
@@ -33,6 +40,7 @@ public class ControllerExceptionAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     protected ApiResponse<Object> handleException(final Exception e) {
+        slackHelper.sendNotification(e);
         return ApiResponse.error(Error.INTERNAL_SERVER_ERROR);
     }
 
